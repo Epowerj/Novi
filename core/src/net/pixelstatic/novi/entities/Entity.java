de5c16de0b7b97cccccb4f42a88e99c29221490e
BuildTools <arnukren@gmail.com>
2016-02-08 22:22:31 -0500

    Changed Entity class a bit.

--- core/src/net/pixelstatic/novi/entities/Entity.java
+++ core/src/net/pixelstatic/novi/entities/Entity.java
@@ -1,5 +1,11 @@
 package net.pixelstatic.novi.entities;
 
-public class Entity{
-
+public abstract class Entity{
+    static long lastid;
+    long id;
+    float x,y;
+    
+    public Entity(){
+	id = lastid++;
+    }
 }
