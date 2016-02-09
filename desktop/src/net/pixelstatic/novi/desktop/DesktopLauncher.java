9e10d08d98eec255bb0a624c13a2371be9f0c740
epowerj <epowerj@github.com>
2016-02-08 20:06:41 -0500

    Added default LibGDX project

---
+++ desktop/src/net/pixelstatic/novi/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.pixelstatic.novi.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.pixelstatic.novi.Novi;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Novi(), config);
+	}
+}
