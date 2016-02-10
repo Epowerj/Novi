1e940eb4bf8528d20ca7c8db08d2877aa5cc2ace
BuildTools <arnukren@gmail.com>
2016-02-09 22:13:44 -0500

    This one is mostly a test.

---
+++ core/src/net/pixelstatic/novi/server/NoviServer.java
@@ -0,0 +1,11 @@
+package net.pixelstatic.novi.server;
+
+import com.esotericsoftware.kryonet.Server;
+
+public class NoviServer{
+	Server server;
+	
+	public static void main(String[] args){
+		
+	}
+}
--- core/src/net/pixelstatic/novi/server/Server.java
+++
@@ -1,7 +0,0 @@
-package net.pixelstatic.novi.server;
-
-public class Server{
-	public static void main(String[] args){
-		
-	}
-}
