15b293f66b316baee95a394ca0a4058e39e3d209
Anuken <arnukren@gmail.com>
2016-02-15 18:07:59 -0500

    Added Bullet Types.

--- core/src/net/pixelstatic/novi/items/ProjectileType.java
+++ core/src/net/pixelstatic/novi/items/ProjectileType.java
@@ -1,15 +1,16 @@
 package net.pixelstatic.novi.items;
 
 import net.pixelstatic.novi.entities.Bullet;
+import net.pixelstatic.novi.modules.Renderer;
 
 public enum ProjectileType{
 	bullet;
 	
-	int getLifetime(){
+	public int getLifetime(){
 		return 100;
 	}
 	
-	void draw(Bullet bullet){
-		
+	public void draw(Bullet bullet, Renderer renderer){
+		renderer.layer("bullet", bullet.x, bullet.y).setLayer(0.5f).setRotation(bullet.velocity.angle() - 90);
 	}
 }
